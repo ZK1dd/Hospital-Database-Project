7ffde887339d7131060fed8a73095ea5abed6fcb
Kiddzt <kiddzt95@gmail.com>
2019-06-10 19:09:22 -0400

    Added PowerPoint, LogicalModel, updated code

--- HospitalDB.sql
+++ HospitalDB.sql
@@ -1,12 +1,23 @@
 
-
-
-
 -- -----------------------------------------------------
 -- Schema FinalProject
 -- -----------------------------------------------------
 CREATE SCHEMA IF NOT EXISTS `FinalProject` DEFAULT CHARACTER SET utf8 ;
 USE `FinalProject` ;
+
+-- -----------------------------------------------------
+-- Table `FinalProject`.`Suppliers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FinalProject`.`Suppliers` (
+  `supplierID` INT NOT NULL,
+  `Name` VARCHAR(25) NULL,
+  `address` VARCHAR(45) NULL,
+  `email` VARCHAR(25) NULL,
+  `telNo` INT NULL,
+  `faxNo` INT NULL,
+  PRIMARY KEY (`supplierID`))
+ENGINE = InnoDB;
+
 
 -- -----------------------------------------------------
 -- Table `FinalProject`.`Ward`
@@ -18,11 +29,33 @@ CREATE TABLE IF NOT EXISTS `FinalProject`.`Ward` (
   `telNo` INT NULL,
   `numBeds` VARCHAR(45) NULL,
   `chargeNurseID` INT NOT NULL,
-  PRIMARY KEY (`wardNo`),
-  INDEX `staffID_idx` (`chargeNurseID` ASC) VISIBLE,
-  CONSTRAINT `staffID`
-    FOREIGN KEY (`chargeNurseID`)
-    REFERENCES `FinalProject`.`Staff` (`staffID`)
+  PRIMARY KEY (`wardNo`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `FinalProject`.`Supplies`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FinalProject`.`Supplies` (
+  `itemNo` INT NOT NULL,
+  `supplierNo` INT NOT NULL,
+  `name` VARCHAR(25) NULL,
+  `description` VARCHAR(45) NULL,
+  `QTY` INT NULL,
+  `reOrderTime` DATETIME NULL,
+  `unitCost` INT NULL,
+  `wardNo` INT NOT NULL,
+  PRIMARY KEY (`itemNo`),
+  INDEX `supplierNo_idx` (`supplierNo` ASC) VISIBLE,
+  INDEX `wardNo_idx` (`wardNo` ASC) VISIBLE,
+  CONSTRAINT `supplierNo`
+    FOREIGN KEY (`supplierNo`)
+    REFERENCES `FinalProject`.`Suppliers` (`supplierID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `wardNo`
+    FOREIGN KEY (`wardNo`)
+    REFERENCES `FinalProject`.`Ward` (`wardNo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -53,155 +86,6 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `FinalProject`.`Patient`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FinalProject`.`Patient` (
-  `patientID` INT NOT NULL,
-  `Fname` VARCHAR(10) NULL,
-  `lName` VARCHAR(10) NULL,
-  `address` VARCHAR(45) NULL,
-  `telNo` INT NULL,
-  `DOB` DATETIME NULL,
-  `gender` VARCHAR(1) NULL,
-  `marStatus` VARCHAR(10) NULL,
-  `dateRegistered` DATETIME NULL,
-  `docID` INT NOT NULL,
-  `clinicNO` INT NOT NULL,
-  PRIMARY KEY (`patientID`),
-  INDEX `staffID_idx` (`docID` ASC) VISIBLE,
-  INDEX `clinicID_idx` (`clinicNO` ASC) VISIBLE,
-  CONSTRAINT `staffID`
-    FOREIGN KEY (`docID`)
-    REFERENCES `FinalProject`.`Doctor` (`staffNo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `clinicID`
-    FOREIGN KEY (`clinicNO`)
-    REFERENCES `FinalProject`.`Clinic` (`clinicNo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `FinalProject`.`Appointments`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FinalProject`.`Appointments` (
-  `appID` INT NOT NULL,
-  `staffNo` INT NOT NULL,
-  `PatientNo` INT NOT NULL,
-  `date` DATETIME NOT NULL,
-  `room` VARCHAR(5) NULL,
-  PRIMARY KEY (`appID`),
-  INDEX `staffID_idx` (`staffNo` ASC) VISIBLE,
-  INDEX `patientNo_idx` (`PatientNo` ASC) VISIBLE,
-  CONSTRAINT `staffID`
-    FOREIGN KEY (`staffNo`)
-    REFERENCES `FinalProject`.`Staff` (`staffID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `patientNo`
-    FOREIGN KEY (`PatientNo`)
-    REFERENCES `FinalProject`.`Patient` (`patientID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `FinalProject`.`Clinic`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FinalProject`.`Clinic` (
-  `clinicNo` INT NOT NULL,
-  `date` DATETIME NULL,
-  `time` DATETIME NULL,
-  `appNo` INT NOT NULL,
-  PRIMARY KEY (`clinicNo`),
-  INDEX `appID_idx` (`appNo` ASC) VISIBLE,
-  CONSTRAINT `appID`
-    FOREIGN KEY (`appNo`)
-    REFERENCES `FinalProject`.`Appointments` (`appID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `FinalProject`.`Doctor`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FinalProject`.`Doctor` (
-  `staffNo` INT NOT NULL,
-  `Name` VARCHAR(30) NULL,
-  `clinicNo` INT NOT NULL,
-  `telNo` INT NULL,
-  PRIMARY KEY (`staffNo`),
-  INDEX `clinicNo_idx` (`clinicNo` ASC) VISIBLE,
-  CONSTRAINT `clinicNo`
-    FOREIGN KEY (`clinicNo`)
-    REFERENCES `FinalProject`.`Clinic` (`clinicNo`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `FinalProject`.`Suppliers`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FinalProject`.`Suppliers` (
-  `supplierID` INT NOT NULL,
-  `Name` VARCHAR(25) NULL,
-  `address` VARCHAR(45) NULL,
-  `email` VARCHAR(25) NULL,
-  `telNo` INT NULL,
-  `faxNo` INT NULL,
-  PRIMARY KEY (`supplierID`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `FinalProject`.`Items`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FinalProject`.`Items` (
-  `itemNo` INT NOT NULL,
-  `supplierNo` INT NOT NULL,
-  `name` VARCHAR(25) NULL,
-  `description` VARCHAR(45) NULL,
-  `QTY` INT NULL,
-  `reOrderTime` DATETIME NULL,
-  `unitCost` INT NULL,
-  PRIMARY KEY (`itemNo`),
-  INDEX `supplierNo_idx` (`supplierNo` ASC) VISIBLE,
-  CONSTRAINT `supplierNo`
-    FOREIGN KEY (`supplierNo`)
-    REFERENCES `FinalProject`.`Suppliers` (`supplierID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `FinalProject`.`Drugs`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `FinalProject`.`Drugs` (
-  `drugNo` INT NOT NULL,
-  `supplierNo` INT NOT NULL,
-  `name` VARCHAR(25) NULL,
-  `Description` VARCHAR(45) NULL,
-  `Dosage` VARCHAR(45) NULL,
-  `QTY` INT NULL,
-  `reOrderTime` DATETIME NULL,
-  `unitCost` INT NULL,
-  PRIMARY KEY (`drugNo`),
-  INDEX `supplierNo_idx` (`supplierNo` ASC) VISIBLE,
-  CONSTRAINT `supplierNo`
-    FOREIGN KEY (`supplierNo`)
-    REFERENCES `FinalProject`.`Suppliers` (`supplierID`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `FinalProject`.`PurchaseReqs`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `FinalProject`.`PurchaseReqs` (
@@ -224,11 +108,6 @@ CREATE TABLE IF NOT EXISTS `FinalProject`.`PurchaseReqs` (
     FOREIGN KEY (`wardNo`)
     REFERENCES `FinalProject`.`Ward` (`wardNo`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `drugNo`
-    FOREIGN KEY ()
-    REFERENCES `FinalProject`.`Drugs` ()
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
@@ -242,17 +121,23 @@ CREATE TABLE IF NOT EXISTS `FinalProject`.`Beds` (
   `wardNo` INT NOT NULL,
   PRIMARY KEY (`bedNo`),
   INDEX `wardNO_idx` (`wardNo` ASC) VISIBLE,
-  INDEX `patientNO_idx` (`patientNo` ASC) VISIBLE,
   CONSTRAINT `wardNO`
     FOREIGN KEY (`wardNo`)
     REFERENCES `FinalProject`.`Ward` (`wardNo`)
     ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `patientNO`
-    FOREIGN KEY (`patientNo`)
-    REFERENCES `FinalProject`.`Patient` (`patientID`)
-    ON DELETE NO ACTION
     ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `FinalProject`.`Clinic`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FinalProject`.`Clinic` (
+  `clinicNo` INT NOT NULL,
+  `date` DATETIME NULL,
+  `time` DATETIME NULL,
+  `appNo` INT NOT NULL,
+  PRIMARY KEY (`clinicNo`))
 ENGINE = InnoDB;
 
 
@@ -265,13 +150,158 @@ CREATE TABLE IF NOT EXISTS `FinalProject`.`NoK` (
   `nokID` INT NOT NULL,
   `address` VARCHAR(45) NULL,
   `relation` VARCHAR(10) NULL,
-  PRIMARY KEY (`nokID`),
-  INDEX `patientID_idx` (`patientNo` ASC) VISIBLE,
-  CONSTRAINT `patientID`
-    FOREIGN KEY (`patientNo`)
-    REFERENCES `FinalProject`.`Patient` (`patientID`)
+  PRIMARY KEY (`nokID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `FinalProject`.`Drugs`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FinalProject`.`Drugs` (
+  `drugNo` INT NOT NULL,
+  `supplierNo` INT NOT NULL,
+  `name` VARCHAR(25) NULL,
+  `Description` VARCHAR(45) NULL,
+  `Dosage` VARCHAR(45) NULL,
+  `QTY` INT NULL,
+  `reOrderTime` DATETIME NULL,
+  `unitCost` INT NULL,
+  `wardNo` INT NOT NULL,
+  PRIMARY KEY (`drugNo`),
+  INDEX `supplierNo_idx` (`supplierNo` ASC) VISIBLE,
+  INDEX `wardNo_idx` (`wardNo` ASC) VISIBLE,
+  CONSTRAINT `supplierNo`
+    FOREIGN KEY (`supplierNo`)
+    REFERENCES `FinalProject`.`Suppliers` (`supplierID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `wardNo`
+    FOREIGN KEY (`wardNo`)
+    REFERENCES `FinalProject`.`Ward` (`wardNo`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
+
+-- -----------------------------------------------------
+-- Table `FinalProject`.`Patient`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FinalProject`.`Patient` (
+  `patientID` INT NOT NULL,
+  `Fname` VARCHAR(10) NULL,
+  `lName` VARCHAR(10) NULL,
+  `address` VARCHAR(45) NULL,
+  `telNo` INT NULL,
+  `DOB` DATETIME NULL,
+  `gender` VARCHAR(1) NULL,
+  `marStatus` VARCHAR(10) NULL,
+  `dateRegistered` DATETIME NULL,
+  `staffID` INT NOT NULL,
+  `clinicNO` INT NOT NULL,
+  `nokID` INT NOT NULL,
+  `allergies` VARCHAR(45) NULL,
+  `drugNo` INT NULL,
+  `medicine` VARCHAR(45) NULL,
+  `unitsperDay` INT NULL,
+  PRIMARY KEY (`patientID`),
+  INDEX `clinicID_idx` (`clinicNO` ASC) VISIBLE,
+  INDEX `fk_Patient_NoK1_idx` (`nokID` ASC) VISIBLE,
+  INDEX `staffID_idx` (`staffID` ASC) VISIBLE,
+  INDEX `drugNo_idx` (`drugNo` ASC) VISIBLE,
+  CONSTRAINT `clinicID`
+    FOREIGN KEY (`clinicNO`)
+    REFERENCES `FinalProject`.`Clinic` (`clinicNo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Patient_NoK1`
+    FOREIGN KEY (`nokID`)
+    REFERENCES `FinalProject`.`NoK` (`nokID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `staffID`
+    FOREIGN KEY (`staffID`)
+    REFERENCES `FinalProject`.`Staff` (`staffID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `drugNo`
+    FOREIGN KEY (`drugNo`)
+    REFERENCES `FinalProject`.`Drugs` (`drugNo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `FinalProject`.`Appointments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FinalProject`.`Appointments` (
+  `appID` INT NOT NULL,
+  `staffNo` INT NOT NULL,
+  `PatientNo` INT NOT NULL,
+  `date` DATETIME NOT NULL,
+  `room` VARCHAR(5) NULL,
+  `clinicNo` INT NOT NULL,
+  PRIMARY KEY (`appID`),
+  INDEX `staffID_idx` (`staffNo` ASC) VISIBLE,
+  INDEX `patientNo_idx` (`PatientNo` ASC) VISIBLE,
+  INDEX `fk_Appointments_Clinic1_idx` (`clinicNo` ASC) VISIBLE,
+  CONSTRAINT `staffID`
+    FOREIGN KEY (`staffNo`)
+    REFERENCES `FinalProject`.`Staff` (`staffID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `patientNo`
+    FOREIGN KEY (`PatientNo`)
+    REFERENCES `FinalProject`.`Patient` (`patientID`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_Appointments_Clinic1`
+    FOREIGN KEY (`clinicNo`)
+    REFERENCES `FinalProject`.`Clinic` (`clinicNo`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+USE `FinalProject` ;
+
+-- -----------------------------------------------------
+-- Placeholder table for view `FinalProject`.`view1`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FinalProject`.`view1` (`id` INT);
+
+-- -----------------------------------------------------
+-- Placeholder table for view `FinalProject`.`view2`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `FinalProject`.`view2` (`id` INT);
+
+-- -----------------------------------------------------
+-- View `FinalProject`.`view1`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `FinalProject`.`view1`;
+USE `FinalProject`;
+CREATE  OR REPLACE VIEW `view1` AS Select *;
+
+-- -----------------------------------------------------
+-- View `FinalProject`.`view2`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `FinalProject`.`view2`;
+USE `FinalProject`;
+CREATE  OR REPLACE VIEW `view2` AS Select * From Appointments,Beds,Clinic,Drugs,NoK,Patient,PurchaseReqs,Staff,Suppliers,Supplies,Ward
+Where Staff.wardNo = Ward.wardNo
+IF Staff.position = "ChargeNurse"
+END IF;
+USE `FinalProject`;
+
+DELIMITER $$
+USE `FinalProject`$$
+CREATE DEFINER = CURRENT_USER TRIGGER `FinalProject`.`Patient_AFTER_UPDATE` AFTER UPDATE ON `Patient` FOR EACH ROW
+BEGIN
+if patient.allergies = patient.medicine
+THEN
+	Delete patient.medicine From patient;
+End if;
+END$$
+
+
+DELIMITER ;
 
